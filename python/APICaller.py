5e50d83d714432d7dfeab61e45c0c66d16c11ad1
Mindia Chubinidze <mi.chubinidze@gmail.com>
2018-03-26 01:41:26 +0400

    implementing Python Library

---
+++ python/APICaller.py
@@ -0,0 +1,27 @@
+
+from LookupRequest import LookupRequest
+from PeptideCardRequest import PeptideCardRequest
+from SearchRequest import SearchRequest
+from RankingSearchRequest import RankingSearchRequest
+
+# lookupRequest = LookupRequest()
+# lookupRequest.lookupType = "medium";
+# print (lookupRequest.request())
+
+
+# peptideCardRequest = PeptideCardRequest()
+# peptideCardRequest.peptide_id = 8;
+# print (peptideCardRequest.request())
+
+
+# searchRequest = SearchRequest()
+# searchRequest.name = "a"
+# print (searchRequest.request())
+
+
+# rankingSearchRequest = RankingSearchRequest()
+# rankingSearchRequest.target_species_id = 3232
+# rankingSearchRequest.activity_measure_id = 72
+# rankingSearchRequest.activity = 100;
+# rankingSearchRequest.operation = ">";
+# print (rankingSearchRequest.request())
